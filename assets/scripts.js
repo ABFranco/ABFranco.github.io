d7df0b0abf85759457a3de8caa8d38574a66132d
abfranco <abfranco4@gmail.com>
2019-01-03 09:45:42 -0800

    button style switch

--- assets/scripts.js
+++ assets/scripts.js
@@ -8,29 +8,60 @@ $(document).ready(function() {
 
   window.addEventListener("scroll", leftChoose);
 
+  // left section text change
   function leftChoose() {
     var scrollTop = $(window).scrollTop() + window.innerHeight / 3;
     var height = window.innerHeight;
 
-    console.log(scrollTop);
-    console.log(height);
-
-    let leftElements = document.getElementsByClassName("left-show");
-    for (var i in leftElements) {
-      leftElements[i].className = "left-hide";
-    }
-    if (scrollTop <= height * 1) {
-      welcomeLeft.className = "left-show";
-    } else if (scrollTop > height * 1 && scrollTop <= height * 2) {
-      aboutLeft.className = "left-show";
-    } else if (scrollTop > height * 2 && scrollTop <= height * 3) {
-      skillsLeft.className = "left-show";
-    } else if (scrollTop > height * 3 && scrollTop <= height * 4) {
-      projectsLeft.className = "left-show";
-    } else if (scrollTop > height * 4) {
-      contactLeft.className = "left-show";
+    if (scrollTop <= height * 1 && !$("#welcome-left").hasClass("left-show")) {
+      $(".left-show").fadeOut(150);
+      $(".left-show").removeClass("left-show");
+      $("#welcome-left").addClass("left-show");
+      $("#welcome-left").fadeIn(150);
+    } else if (
+      scrollTop > height * 1 &&
+      scrollTop <= height * 2 &&
+      !$("#about-left").hasClass("left-show")
+    ) {
+      $(".left-show").fadeOut(150);
+      $(".left-show").removeClass("left-show");
+      $("#about-left").addClass("left-show");
+      $("#about-left").fadeIn(150);
+    } else if (
+      scrollTop > height * 2 &&
+      scrollTop <= height * 3 &&
+      !$("#skills-left").hasClass("left-show")
+    ) {
+      $(".left-show").fadeOut(150);
+      $(".left-show").removeClass("left-show");
+      $("#skills-left").addClass("left-show");
+      $("#skills-left").fadeIn(150);
+      $("#skills-left").css("display", "flex");
+    } else if (
+      scrollTop > height * 3 &&
+      scrollTop <= height * 4 &&
+      !$("#projects-left").hasClass("left-show")
+    ) {
+      $(".left-show").fadeOut(150);
+      $(".left-show").removeClass("left-show");
+      $("#projects-left").addClass("left-show");
+      $("#projects-left").fadeIn(150);
+    } else if (
+      scrollTop > height * 4 &&
+      !$("#contact-left").hasClass("left-show")
+    ) {
+      $(".left-show").fadeOut(150);
+      $(".left-show").removeClass("left-show");
+      $("#contact-left").addClass("left-show");
+      $("#contact-left").fadeIn(150);
     }
   }
+
+  // skills section buttons hover
+  $(".skills-button").mouseover(function() {
+    $(".button-on").removeClass("button-on");
+    $(this).addClass("button-on");
+  });
 
   // smooth scroll effct
 
