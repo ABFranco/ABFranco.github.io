7c90fd7ecd1aaa8b0776b476bfa7b3932fc6ea6c
ABFranco <abfranco4@gmail.com>
2023-11-18 17:49:51 -0800

    add home page and NavigationBar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './pages/Home';
+import NavigationBar from './components/NavigationBar';
 
-function App() {
+// const Home = () => {
+//   return (
+//     <div>
+//       <h1>Welcome to the Home Page</h1>
+//       <p>This is your home page content.</p>
+//     </div>
+//   );
+// };
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Hello Antonio! The website is up and working.
         </p>
+      </header> */}
+      <header>
+        <NavigationBar />
       </header>
+      <main>
+        <Home />
+      </main>
+      <footer>
+        <p>© 2023 My React App</p>
+      </footer>
     </div>
   );
 }
