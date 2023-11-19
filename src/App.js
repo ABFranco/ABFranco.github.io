6dc0001e38eb5d4915250743023e1ec1ff32968b
ABFranco <abfranco4@gmail.com>
2023-11-18 17:51:56 -0800

    remove old default App page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 import Home from './pages/Home';
 import NavigationBar from './components/NavigationBar';
 
-// const Home = () => {
-//   return (
-//     <div>
-//       <h1>Welcome to the Home Page</h1>
-//       <p>This is your home page content.</p>
-//     </div>
-//   );
-// };
-
 const App = () => {
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello Antonio! The website is up and working.
-        </p>
-      </header> */}
       <header>
         <NavigationBar />
       </header>
@@ -28,7 +12,7 @@ const App = () => {
         <Home />
       </main>
       <footer>
-        <p>© 2023 My React App</p>
+        <p>© {new Date().getFullYear()} Antonio Franco</p>
       </footer>
     </div>
   );
