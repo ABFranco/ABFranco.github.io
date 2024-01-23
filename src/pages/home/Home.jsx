e60a820e6c34a8e1b4085cdfe75c5b6375449eb1
ABFranco <abfranco4@gmail.com>
2024-01-22 21:25:32 -0800

    adjust some css

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
   return (
     <div className="home-container">
       <div className="home-text">
-        <h1>Hello! I'm Antonio Franco. 👋🏽</h1>
+        <h1>Hello! I'm Antonio. 👋🏽</h1>
         <h2>Software Engineer and Running Enthusiast.</h2>
       </div>
       <div className="home-img">
