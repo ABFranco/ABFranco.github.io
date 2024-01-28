a85c27b7eb1129241efc24981111e6209f1f0114
Antonio Franco <abfranco4@gmail.com>
2024-01-27 16:58:00 -0800

    add material ui theme

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import "./App.css";
+import { ThemeProvider } from "@mui/material/styles";
 import Home from "./pages/home/Home.jsx";
 import About from "./components/About/About.jsx";
 import NavigationBar from "./components/NavigationBar";
 import Footer from "./components/Footer";
+import theme from "./theme/theme.js";
 
 const App = () => {
   return (
-    <div className="App">
-      <header>
-        <NavigationBar />
-      </header>
-      <main>
-        <Home />
-        <About />
-      </main>
-      <Footer />
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <header>
+          <NavigationBar />
+        </header>
+        <main>
+          <Home />
+          <About />
+        </main>
+        <Footer />
+      </div>
+    </ThemeProvider>
   );
 };
 
