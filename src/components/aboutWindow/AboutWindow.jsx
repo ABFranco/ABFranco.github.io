d4a88deb9762e445ce46b45f599c93bbda2c73c1
ABFranco <abfranco4@gmail.com>
2024-02-02 19:12:47 -0800

    add links to website in About

--- src/components/aboutWindow/AboutWindow.jsx
+++ src/components/aboutWindow/AboutWindow.jsx
@@ -1,8 +1,22 @@
 import React from "react";
 import "./aboutWindow.css";
-import { Typography } from "@mui/material";
+import { Typography, Link } from "@mui/material";
 
 const AboutWindow = () => {
+
+  const websiteLink = (text, url) => (
+    <Link
+      href={url}
+      target="_blank"
+      rel="noopener noreferrer"
+      underline="none"
+      color="inherit"
+      fontWeight="600"
+    >
+      {text}
+    </Link>
+  );
+
   return (
     <div className="about-window">
       <div className="window-bar">
@@ -13,10 +27,10 @@ const AboutWindow = () => {
 
       <div className="about-content">
         <Typography variant="body1">
-          My name is Antonio, and I am currently a Software Engineer at Hall Capital Partners. Previously, I worked at Inductive Automation and CITRIS @ UC Berkeley. My academic background includes studying Computer Science in UC Berkeley's Engineering department.
+          My name is Antonio, and I am currently a Software Engineer at {websiteLink("Hall Capital Partners", "https://www.hallcapital.com/")}. Previously, I worked at {websiteLink("Inductive Automation", "https://inductiveautomation.com/")} and {websiteLink("CITRIS @ UC Berkeley", "https://citris-uc.org/")}. My academic background includes studying {websiteLink("Computer Science in UC Berkeley's", "https://eecs.berkeley.edu/")} Engineering department.
         </Typography>
         <Typography variant="body1">
-          I am passionate for programming and building innovative solutions. My career aspirations mainly include web development, software engineering, and UX design. I aim for continuous personal growth, driven by the desire to learn and adapt to new technologies and best practices in the ever-evolving landscape of the tech industry.
+          I am passionate for programming and building innovative solutions. My career aspirations mainly include web development, software engineering, and UX design. I aim for continuous personal growth, driven by the desire to learn new technologies and best practices in the ever-evolving landscape of the tech industry.
         </Typography>
         <Typography variant="body1">
           On my free time, I love to run. I'm often training for an upcoming marathon, or looking for the next running shoe I should buy! 😅
